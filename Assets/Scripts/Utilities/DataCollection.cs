202bd543029d75c1ae68f6d748120f4fbb1eef0d
dadavidtseng <dadavidtseng@gmail.com>
2023-02-28 03:32:30 +0800

    lessosn11 背包數據初始化 complete 1.ItemDetails詳情變量信息 2.Enums創建ItemType物品類型 3.生成ItemDataList_SO文件作為整個遊戲的物品管理數據庫

---
+++ Assets/Scripts/Utilities/DataCollection.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[System.Serializable]
+
+public class ItemDetails
+{
+    public int itemID;
+    public string name;
+    public ItemType itemType;
+    public Sprite itemIcon;
+    public Sprite itemOnWorldSprite;
+    public string itemDescription;
+    public int itemUseRadius;
+    public bool canPickup;
+    public bool canDropped;
+    public bool canCarried;
+    public int itemPrice;
+    [Range(0, 1)]
+    public float sellPercentage;
+}
