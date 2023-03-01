50297d898ac4f8a23604b4bbed27824ec2221c6e
dadavidtseng <dadavidtseng@gmail.com>
2023-03-01 08:51:28 +0800

    lesson14 (Editor)生成ListView列表 complete

--- Assets/Editor/UI Builder/ItemEditor.cs
+++ Assets/Editor/UI Builder/ItemEditor.cs
@@ -2,10 +2,19 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using System.Collections.Generic;
+using System;
 
 
 public class ItemEditor : EditorWindow
 {
+    private ItemDataList_SO dataBase;
+    private List<ItemDetails> itemList = new List<ItemDetails>();
+    private VisualTreeAsset itemRowTemplate;
+    
+    //獲得VisualElement
+    private ListView itemListView;
+
     [MenuItem("M Studio/ItemEditor")]
     public static void ShowExample()
     {
@@ -33,5 +42,56 @@ public class ItemEditor : EditorWindow
         // VisualElement labelWithStyle = new Label("Hello World! With Style");
         // labelWithStyle.styleSheets.Add(styleSheet);
         // root.Add(labelWithStyle);
+        
+        //拿到模板數據
+        itemRowTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Builder/ItemRowTemplate.uxml");
+        
+        //變量賦值
+        itemListView = root.Q<VisualElement>("ItemList").Q<ListView>("ListView");
+        
+        //加載數據
+        LoadDataBase();
+        
+        //生成ListView
+        GenerateListView();
+    }
+
+    private void LoadDataBase()
+    {
+        var dataArray = AssetDatabase.FindAssets("ItemDataList_SO");
+
+        if (dataArray.Length > 1)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(dataArray[0]);
+            dataBase = AssetDatabase.LoadAssetAtPath(path, typeof(ItemDataList_SO)) as ItemDataList_SO;
+        }
+
+        itemList = dataBase.itemDetailsList;
+        //如果不標記則無法保存數據
+        EditorUtility.SetDirty(dataBase);
+        // Debug.Log(itemList[0].itemID);
+    }
+
+    private void GenerateListView()
+    {
+        Func<VisualElement> makeItem = () => itemRowTemplate.CloneTree();
+
+        Action<VisualElement, int> bindItem = (e, i) =>
+        {
+            if (i < itemList.Count)
+            {
+                if (itemList[i].itemIcon != null)
+                {
+                    e.Q<VisualElement>("Icon").style.backgroundImage = itemList[i].itemIcon.texture;
+                }
+
+                e.Q<Label>("Name").text = itemList[i] == null ? "NO ITEm" : itemList[i].itemName;
+            }
+        };
+
+        itemListView.fixedItemHeight = 60;
+        itemListView.itemsSource = itemList;
+        itemListView.makeItem = makeItem;
+        itemListView.bindItem = bindItem;
     }
 }
