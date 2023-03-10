2e8eeb0a8727a4f374e8c9606af00e0f009f46d4
dadavidtseng <dadavidtseng@gmail.com>
2023-03-10 17:10:01 +0800

    lesson39 創建TransitionManager控制人物場景切換 complete

---
+++ Assets/Scripts/Transition/TransitionManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MFarm.Transition
+{
+    public class TransitionManager : MonoBehaviour
+    {
+        public string startScneeName = string.Empty;
+
+        private void OnEnable()
+        {
+            EventHandler.TransitionEvent += OnTransitionEvent;
+        }
+
+        private void OnDisable()
+        {
+            EventHandler.TransitionEvent -= OnTransitionEvent;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(LoadSceneSetActive(startScneeName));
+        }
+        
+        private void OnTransitionEvent(string sceneToGo, Vector3 positionToGo)
+        {
+            StartCoroutine(Transition(sceneToGo, positionToGo));
+        }
+
+        /// <summary>
+        /// 場景切換
+        /// </summary>
+        /// <param name="sceneName">目標場景</param>
+        /// <param name="targetPosition">目標位置</param>
+        /// <returns></returns>
+        private IEnumerator Transition(string sceneName, Vector3 targetPosition)
+        {
+            yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+
+            yield return LoadSceneSetActive(sceneName);
+        }
+
+        /// <summary>
+        /// 加載場景並設置為激活
+        /// </summary>
+        /// <param name="sceneName">場景名</param>
+        /// <returns></returns>
+        private IEnumerator LoadSceneSetActive(string sceneName)
+        {
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+            Scene newScene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+
+            SceneManager.SetActiveScene(newScene);
+        }
+    }
+}
